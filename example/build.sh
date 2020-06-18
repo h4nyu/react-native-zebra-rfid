965b9d910134f9ff2467b48fa5af9961f613ced7
x1nyuan <yao.ntno@gmail.com>
2020-06-18 15:26:10 +0900

    fix app name

--- example/build.sh
+++ example/build.sh
@@ -1,7 +1,7 @@
 # yarn react-native link
-# yarn react-native bundle --platform android --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/ $@
-
-# yarn jetify
+yarn react-native bundle --platform android --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/ $@
+yarn jetify
 cd android
 # keytool -genkey -v -keystore app/debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000 -dname "C=US, O=Android, CN=Android Debug"
+./gradlew clean
 ./gradlew assembleDebug
