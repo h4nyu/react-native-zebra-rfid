c252658dcd188f88576220fcde409021bd5f19fd
x1nyuan <yao.ntno@gmail.com>
2020-06-23 15:23:02 +0900

    remove files

--- example/build.sh
+++
@@ -1,5 +0,0 @@
-npx react-native bundle --platform android --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/ $@ \
-&& npx jetify \
-&& cd android \
-&& ./gradlew assembleDebug --info
-
